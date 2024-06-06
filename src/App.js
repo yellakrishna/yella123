ff76bebab18d7828686e736342f1119f587df731
yellakrishna <yellakrishna7675@gmail.com>
2024-06-06 14:08:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src='fishbg.png' className="App-logo" alt="logo" />
+        <img src='yellakrishna.jpg' className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
